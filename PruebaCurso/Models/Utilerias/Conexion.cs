51a065661b6d797118eb78a20a96b75ed47cc498
Marco Aguilar <marcocaaguilar@gmail.com>
2019-10-23 15:45:42 -0500

    Prueba commit 2 carpetas

---
+++ PruebaCurso/Models/Utilerias/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PruebaCurso.Models.Utilerias
+{
+    public class Conexion
+    {
+        static readonly string Server = "MCarranza";
+        static readonly string Db = "Productos";
+        static readonly string User = "sa";
+        static readonly string Pass = "123456";
+
+        protected SqlConnection conexion { get; set; }
+
+        protected SqlConnection Conectar()
+        {
+            try
+            {
+                conexion = new SqlConnection("Data Source=" + Server + ";Initial Catalog=" + Db + ";User ID=" + User + ";Password=" + Pass + ";Integrated Security=False;");
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
